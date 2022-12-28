3c8e1d5c2fb8499af80323e37890652b6ae11c2c
Blank67 <hmnshshrm67@gmail.com>
2022-12-28 22:55:07 +0530

    first test

---
+++ src/components/Greetings.js
@@ -0,0 +1,10 @@
+const Greetings = () => {
+    return (
+        <div>
+            <h1>Welcome User</h1>
+            <p>You are logged in.</p>
+        </div>
+    );
+}
+
+export default Greetings;
---
+++ src/components/Greetings.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react"
+import Greetings from "./Greetings"
+
+test('renders Welcome User as a text', () => {
+    //Arrange test
+    render(<Greetings />)
+
+    //Act
+    //...for now do nothing
+
+    //Assert
+    const element = screen.getByText('Welcome user', {exact: false}); //excat false to ignore case 
+    expect(element).toBeInTheDocument();
+})
