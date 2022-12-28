644216cf123daf9f0da6e05394e4b59a67db95d5
Blank67 <hmnshshrm67@gmail.com>
2022-12-28 22:56:21 +0530

    test suites

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Greetings from "./Greetings"
 
-test('renders Welcome User as a text', () => {
-    //Arrange test
-    render(<Greetings />)
+describe('Greeting component tests', () => {
+    test('renders Welcome User as a text', () => {
+        //Arrange test
+        render(<Greetings />)
 
-    //Act
-    //...for now do nothing
+        //Act
+        //...for now do nothing
 
-    //Assert
-    const element = screen.getByText('Welcome user', {exact: false}); //excat false to ignore case 
-    expect(element).toBeInTheDocument();
+        //Assert
+        const element = screen.getByText('Welcome user', { exact: false }); //excat false to ignore case 
+        expect(element).toBeInTheDocument();
+    })
 })
