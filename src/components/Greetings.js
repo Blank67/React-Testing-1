a51f9641f4dc5be8f04d87b090404304cca2f302
Blank67 <hmnshshrm67@gmail.com>
2022-12-29 09:47:55 +0530

    tests using state

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greetings = () => {
+    const [text, setText] = useState(false);
+    
+    const toggleTextHandler = () => {
+        setText(true);
+    } 
+    
     return (
         <div>
             <h1>Welcome User</h1>
-            <p>You are logged in.</p>
+            {!text && <p>You are logged in.</p>}
+            {text && <p>Text Changed.</p>}
+            <button onClick={toggleTextHandler}>Toggle Text</button>
         </div>
     );
 }
--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Greetings from "./Greetings"
 
 describe('Greeting component tests', () => {
     test('renders Welcome User as a text', () => {
         //Arrange test
-        render(<Greetings />)
+        render(<Greetings />);
 
         //Act
         //...for now do nothing
@@ -12,5 +13,35 @@ describe('Greeting component tests', () => {
         //Assert
         const element = screen.getByText('Welcome user', { exact: false }); //excat false to ignore case 
         expect(element).toBeInTheDocument();
+    })
+
+    test('renders logged in if button is not clicked', () => {
+        render(<Greetings />);
+
+        const element = screen.getByText('You are logged in.', { exact: false });
+        expect(element).toBeInTheDocument();
+    })
+
+    test('renders changed when button is clicked', () => {
+        //Arrange
+        render(<Greetings />);
+
+        //Act
+        const button = screen.getByRole('button');
+        userEvent.click(button);
+
+        //Asert
+        const element = screen.getByText('Changed.', { exact: false });
+        expect(element).toBeInTheDocument();
+    })
+
+    test('does not renders logged in if button is clicked', () => {
+        render(<Greetings />);
+
+        const button = screen.getByRole('button');
+        userEvent.click(button);
+
+        const element = screen.queryByText('You are logged in.', { exact: false }); //because getByText throw error if element is not found
+        expect(element).not.toBeInTheDocument();
     })
 })
